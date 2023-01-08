33b2fed9d2b9ebb6d70080f6e06d2855053d56d9
master <fachri.fajar@gmail.com>
2023-01-09 02:11:26 +0700

    [add] components Navbar (detail-recipe page)

--- src/pages/detail-recipe.js
+++ src/pages/detail-recipe.js
@@ -3,41 +3,10 @@ import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import "../styles/detail-recipe.css";
 import "../styles/mobile-detail-recipe.css";
+import Navbar from "../components/navbar";
 
 const DetailRecipe = () => {
   React.useEffect(() => {
-    // Add animations to navbar (disappear while scroll & show while stop and scrolling to top)
-    let previousScrollPosition = window.scrollY;
-
-    window.addEventListener("scroll", function () {
-      const navbar = document.querySelector("#navbar-desktop");
-      const currentScrollPosition = window.scrollY;
-      if (currentScrollPosition > previousScrollPosition) {
-        // User is scrolling down
-        navbar.classList.add("hidden");
-      } else {
-        // User is scrolling up
-        navbar.classList.remove("hidden");
-      }
-      previousScrollPosition = currentScrollPosition;
-
-      if (currentScrollPosition === 0) {
-        // User has scrolled to the top of the page
-        navbar.style.backgroundColor = "transparent";
-        navbar.style.backdropFilter = "none";
-        navbar.style.boxShadow = "none";
-      } else {
-        // overridden css function
-        navbar.style.backgroundColor = "rgba(255,255,255,0.8)";
-        navbar.style.backdropFilter = "blur(5px)";
-      }
-
-      setTimeout(function () {
-        navbar.classList.remove("hidden");
-      }, 1500);
-    });
-    // End of animations to navbar (disappear while scroll & show while stop and scrolling to top)
-
     // add alert to button icons
     const buttonIcons = document.querySelector(".icon-link");
 
@@ -70,37 +39,7 @@ const DetailRecipe = () => {
 
       <section id="detail-recipe-desktop-version">
         {/* <!-- ! Navbar --> */}
-        <nav className="navbar navbar-expand-md fixed-top" id="navbar-desktop">
-          <div className="container">
-            <button
-              className="navbar-toggler"
-              data-bs-toggle="collapse"
-              data-bs-target="#nav"
-              aria-controls="nav"
-              aria-label="Expand Navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="nav">
-              <ul className="navbar-nav">
-                <li className="nav-item me-5">
-                  <Link to="/" className="nav-link" aria-current="page">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item me-5">
-                  <Link to="/add-recipe" className="nav-link">
-                    Add Recipe
-                  </Link>
-                </li>
-                <li className="nav-item me-5">
-                  <Link to="/profile" className="nav-link">
-                    Profile
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar />
 
         {/* <!-- ! MAIN IMAGE --> */}
         <section id="selected-recipe">
