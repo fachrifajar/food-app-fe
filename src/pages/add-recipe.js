3e01ac8f2150678d43a3c27be4bdee0f073a1283
master <fachri.fajar@gmail.com>
2023-01-09 02:38:55 +0700

    [add] components footer (add-recipe page)

--- src/pages/add-recipe.js
+++ src/pages/add-recipe.js
@@ -2,6 +2,7 @@ import { React, useEffect } from "react";
 import Helmet from "react-helmet";
 import "../styles/add-recipe.css";
 import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 function AddRecipe() {
   useEffect(() => {
@@ -132,46 +133,7 @@ function AddRecipe() {
       </section>
 
       {/* <!-- ! footer --> */}
-      <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col-12 text-center">
-              <h2 className="font-weight-bold">Eat, Cook, Repeat</h2>
-              <p className="my-4">Share your best recipe by uploading here!</p>
-              <div className="d-flex justify-content-center social-links">
-                <a
-                  href="https://www.linkedin.com/in/fachri-fajar/"
-                  className="mx-2"
-                  target="_blank"
-                  rel="noreferrer noopener">
-                  <i className="fab fa-linkedin"></i>
-                </a>
-                <a
-                  href="https://www.instagram.com/fachrifajar/?hl=id"
-                  className="mx-2"
-                  target="_blank"
-                  rel="noreferrer noopener">
-                  <i className="fab fa-instagram"></i>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/fachri-fajar/"
-                  className="mx-2"
-                  target="_blank"
-                  rel="noreferrer noopener">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/fachri-fajar/"
-                  className="mx-2"
-                  target="_blank"
-                  rel="noreferrer noopener">
-                  <i className="fab fa-facebook"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
